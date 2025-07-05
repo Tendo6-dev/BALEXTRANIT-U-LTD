d3274212e4f4b0258cb83dcf26dcf12af9e4f14f
Tendo6-dev <rugumayomichael739@gmail.com>
2025-07-05 12:15:33 +0300

    Create NamingConflict.java

---
+++ NamingConflict.java
@@ -0,0 +1,10 @@
+public class NamingConflict {
+    private String patientID;
+    private String allergyNotes;
+    
+    public NamingConflict(String patientID, String allergyNotes) {
+      
+        this.patientID = patientID;
+        this.allergyNotes = allergyNotes;
+    }
+}
