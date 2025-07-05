47664d8619dc4c4737ac42a271b6e7f324b826af
Tendo6-dev <rugumayomichael739@gmail.com>
2025-07-05 10:29:37 +0300

    Update CompanyTest.java

--- CompanyTest.java
+++ CompanyTest.java
@@ -6,7 +6,7 @@ public class CompanyTest {
     @Test
     public void testSchedulePickupWithAvailableVehicle() {
         Company company = new Company();
-        // Add a vehicle to the company
+        
         company.addVehicle(new Taxi("T1", 4));
         
         Passenger passenger = new Passenger(new Location(10, 10), new Location(20, 20));
@@ -16,7 +16,7 @@ public class CompanyTest {
     @Test
     public void testSchedulePickupWithNoAvailableVehicle() {
         Company company = new Company();
-        // Don't add any vehicles
+        
         
         Passenger passenger = new Passenger(new Location(10, 10), new Location(20, 20));
         assertFalse(company.schedulePickup(passenger));
