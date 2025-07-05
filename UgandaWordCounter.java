dea07dc2429b8b4a60a402e553b83d9f2f9ddd75
Tendo6-dev <rugumayomichael739@gmail.com>
2025-07-05 11:31:30 +0300

    Update and rename Counter.java to UgandaWordCounter.java

--- UgandaWordCounter.java
+++ UgandaWordCounter.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Counter {
+public class UgandaWordCounter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
